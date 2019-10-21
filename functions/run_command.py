346cd835e6078c8ad6ee7a4682393813bc057dc8
thaight <haight@uleth.ca>
2019-10-21 09:53:35 -0600

    Create run_command.py

---
+++ functions/run_command.py
@@ -0,0 +1,16 @@
+#!/usr/python
+import sys
+import os
+import subprocess
+import shlex
+
+def run_command(command):
+    process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output:
+            print output.strip()
+    rc = process.poll()
+    return rc
