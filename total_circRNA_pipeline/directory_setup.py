b96c22c999877923a1ec797cdb08dcf95e5a2a17
thaight <haight@uleth.ca>
2022-09-08 13:54:32 -0600

    Delete directory_setup.py

--- total_circRNA_pipeline/directory_setup.py
+++
@@ -1,6 +0,0 @@
-def directory_setup(prefix,path):
-    """ Use: Takes a path and prefix and creates a directory if it doesnt exist """
-    for element in prefix:
-       dir = path + "/" + element
-       if not os.path.isdir(dir):
-          os.makedirs(dir)
