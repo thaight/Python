d9ddc8e74fb3104000e130fb9b081006d99e4b4c
thaight <haight@uleth.ca>
2022-09-08 13:54:26 -0600

    Delete make_executable.py

--- total_circRNA_pipeline/make_executable.py
+++
@@ -1,5 +0,0 @@
-def make_executable(path):
-    """ Use: Takes a script as input and makes it executable """
-    mode = os.stat(path).st_mode
-    mode |= (mode & 0o444) >> 2    # copy R bits to X
-    os.chmod(path, mode)
