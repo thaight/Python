e8e488699b7d14e0c61dd6dfa7e8b70f0f02fbad
thaight <haight@uleth.ca>
2022-11-17 08:15:02 -0700

    Update listmaker.py
    Updated to take prefix instead of 3 different file names

--- scripts/listmaker.py
+++ scripts/listmaker.py
@@ -3,27 +3,23 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Produces a list to be used for the detection and anlysis of circRNA.')
-parser.add_argument("-p", "--path", help="Absolute path to folder containing sample files. Example: /home/user/data/mouse/")
-parser.add_argument("-o", "--output", help="Name of list file to create. Example: test_data.list.")
-parser.add_argument("-g", "--gtf", help="Name of gtf list file to create. Example test_data_gtf.list")
-parser.add_argument("-t", "--geneout", help="Name of geneout list file to create. Example test_data_geneout.list")
+parser.add_argument("-i", "--input", help="Absolute path to folder containing sample files. Example: /home/user/data/mouse/")
+parser.add_argument("-o", "--prefix", help="Name of list file to create. Example: test_data.list.")
 args = parser.parse_args()
-path = args.path
-output = args.output
-gtf = args.gtf
-geneout = args.geneout
-
+path = args.input
+prefix = args.prefix
 dir = "Running listmaker on the following directory: " + path
 print(dir)
-
 #Works on the current directory you are in
 os.chdir(path)
+output = prefix + ".list"
+gtf = prefix + "_gtf.list"
+geneout = prefix + "_geneout.list"
 with open(output, 'w') as write_obj:
         for R1path in next(os.walk('.'))[1]:
                 write_obj.write(R1path)
                 write_obj.write("\n")
         write_obj.close()
-
 with open(gtf, 'w') as write_obj:
         for R1path in next(os.walk('.'))[1]:
                 write_obj.write(R1path)
@@ -32,7 +28,6 @@ with open(gtf, 'w') as write_obj:
                 write_obj.write(gtfpath)
                 write_obj.write("\n")
         write_obj.close()
-
 with open(geneout, 'w') as write_obj:
         for R1path in next(os.walk('.'))[1]:
                 write_obj.write(R1path)
@@ -41,4 +36,3 @@ with open(geneout, 'w') as write_obj:
                 write_obj.write(geneoutpath)
                 write_obj.write("\n")
         write_obj.close()
-
