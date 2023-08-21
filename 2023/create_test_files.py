3c0bf3dc87f590db76c8baa51ee244002fe51745
thaight <haight@uleth.ca>
2023-08-20 18:05:57 -0700

    Update create_test_files.py

--- 2023/create_test_files.py
+++ 2023/create_test_files.py
@@ -1,11 +1,11 @@
 #!/bin/python3
 ''' The following script takes as input an in and out directory, in which the in directory contains files and the out directory is the folder to write out to. The purpose is to create blank files for testi>
-''' Version: 1.0 '''
+''' Version: 2.0 '''
 ''' Date: August 18th, 2023 '''
 ''' Publisher: Travis Haight '''
 import os
 import shutil
-
+import argparse
 def create_blank_files(input_directory, output_directory):
     #Get list of files
     file_names = os.listdir(input_directory)
@@ -22,6 +22,13 @@ def create_blank_files(input_directory, output_directory):
         print(f"Created file: {output_file_path}")
 
 #Usage
-input_directory = '/home/thaight/projects/rrg-zovoilis/thaight/projects/calgary_brain_bank'
-output_directory = '/home/thaight/projects/rrg-zovoilis/thaight/projects/project_test'
-create_blank_files(input_directory, output_directory)
+parser = argparse.ArgumentParser(description='Supply this script with the absolute path to a folder containing samples, and the absolute path to a folder you want to create for testing')
+parser.add_argument('-i', '--input', help='Absolute path to a folder containing samples. Ensure that it does not end with "/" for example: /home/test/project1')
+parse.add_argument('-o', '--output', help='Absolute path to the output folder, Ensure that it does not end with "/" for example: /home/test/project1. Note: It does not need to exist.')
+args = parser.parse_args()
+input_dir = args.input
+output_dir = args.output
+create_blank_files(input_dir, output_dir)
+
+
+
